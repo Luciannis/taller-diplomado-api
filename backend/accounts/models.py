5547758202e75036e82ea724299e99b689376bfb
Luciannis <fcoesteban13@gmail.com>
2023-12-03 21:42:09 -0300

    se asigna una billetera al crear un usuario mas un bono de 50000 clp

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,16 @@
 # accounts/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from wallets.models import Wallet
 
 class User(AbstractUser):
     phone = models.CharField(max_length=13)
+
+    def save(self, *args, **kwargs):
+        is_new_user = not self.pk  # Verifica si el usuario es nuevo
+
+        super().save(*args, **kwargs)  # Guarda el usuario
+
+        if is_new_user:  # Si es un usuario nuevo
+            initial_balance_clp = 50000  # $50,000 de bono de bienvenida
+            Wallet.objects.create(user=self, balance_clp=initial_balance_clp)
--- backend/wallets/models.py
+++ backend/wallets/models.py
@@ -1,9 +1,9 @@
 # wallets/models.py
-from accounts.models import User
+from django.conf import settings
 # from django.contrib.auth.models import User
 from django.db import models
 
 class Wallet(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     balance_btc = models.DecimalField(max_digits=15, decimal_places=8, default=0)
     balance_clp = models.DecimalField(max_digits=15, decimal_places=2, default=0)
